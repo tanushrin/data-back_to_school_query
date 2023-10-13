1a77adbe5c126e3b837586562ee936cec511d9a7
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-13 10:52:39 +0200

    Warm up done.

--- school.py
+++ school.py
@@ -12,9 +12,7 @@ def students_from_city(db, city):
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
-#
-'''
-import sqlite3
-conn = sqlite3.connect('data/school.sqlite')
-db = conn.cursor()
-print(students_from_city(db, 'Paris'))'''
+#   import sqlite3
+# conn = sqlite3.connect('data/school.sqlite')
+# db = conn.cursor()
+# print(students_from_city(db, 'Paris'))
