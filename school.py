f1856321c24f94b279cdba5325c91c2a22026966
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-13 10:51:01 +0200

    Warm up done.

--- school.py
+++ school.py
@@ -2,14 +2,19 @@
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+
+    query = f"""select * from students s where s.birth_city = '{city}'"""
+    db.execute(query)
+    results = db.fetchall()
+    return results
 
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
 #
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+'''
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+db = conn.cursor()
+print(students_from_city(db, 'Paris'))'''
